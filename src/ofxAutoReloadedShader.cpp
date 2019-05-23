1bcc6f35fcf47f7f289eac311bf25262a1fa7166
Akira Hayasaka <akira.hayasaka@gmail.com>
2019-05-23 14:30:12 +0900

    enable auto reload preprocessor #include (#pragma include)

--- src/ofxAutoReloadedShader.cpp
+++ src/ofxAutoReloadedShader.cpp
@@ -3,81 +3,65 @@
 
 
 
-ofxAutoReloadedShader::ofxAutoReloadedShader() : type(TYPE::other) {
-	bWatchingFiles = false;
-}
+ofxAutoReloadedShader::ofxAutoReloadedShader() : type(TYPE::other), bWatchingFiles(false) {}
 
-ofxAutoReloadedShader::~ofxAutoReloadedShader(){
+ofxAutoReloadedShader::~ofxAutoReloadedShader()
+{
 	disableWatchFiles();
 }
 
-// ---------------------------------------------------------------------------------------------------------------------------------------------------
-//
-bool ofxAutoReloadedShader::load(string shaderName )
+bool ofxAutoReloadedShader::load(const std::filesystem::path& shaderName)
 {
-	return load( shaderName + ".vert", shaderName + ".frag", shaderName + ".geom" );
+	return load(shaderName.string() + ".vert", shaderName.string() + ".frag", shaderName.string() + ".geom");
 }
 
-// ---------------------------------------------------------------------------------------------------------------------------------------------------
-//
-bool ofxAutoReloadedShader::load(string vertName, string fragName, string geomName)
+bool ofxAutoReloadedShader::load(const std::filesystem::path& vertName, const std::filesystem::path& fragName, const std::filesystem::path& geomName)
 {
 	unload();
-	
-    ofShader::setGeometryOutputCount(geometryOutputCount);
-    ofShader::setGeometryInputType(geometryInputType);
-    ofShader::setGeometryOutputType(geometryOutputType);
 
-    
+	ofShader::setGeometryOutputCount(geometryOutputCount);
+	ofShader::setGeometryInputType(geometryInputType);
+	ofShader::setGeometryOutputType(geometryOutputType);
+
 	// hackety hack, clear errors or shader will fail to compile
 	GLuint err = glGetError();
-	
+
 	lastTimeCheckMillis = ofGetElapsedTimeMillis();
-	setMillisBetweenFileCheck( 2 * 1000 );
+	setMillisBetweenFileCheck(2 * 1000);
 	enableWatchFiles();
-	
+
 	loadShaderNextFrame = false;
-	
+
 	vertexShaderFilename = vertName;
 	fragmentShaderFilename = fragName;
 	geometryShaderFilename = geomName;
-	
+
 	vertexShaderFile.clear();
 	fragmentShaderFile.clear();
 	geometryShaderFile.clear();
-	
-	vertexShaderFile   = ofFile( ofToDataPath( vertexShaderFilename ) );
-	fragmentShaderFile = ofFile( ofToDataPath( fragmentShaderFilename ) );
-	geometryShaderFile = ofFile( ofToDataPath( geometryShaderFilename ) );
-	
-	ofBuffer vertexShaderBuffer = ofBufferFromFile( ofToDataPath( vertexShaderFilename ) );
-	ofBuffer fragmentShaderBuffer = ofBufferFromFile( ofToDataPath( fragmentShaderFilename ) );
-	ofBuffer geometryShaderBuffer = ofBufferFromFile( ofToDataPath( geometryShaderFilename ) );
-	
+
+	vertexShaderFile = ofFile(ofToDataPath(vertexShaderFilename));
+	fragmentShaderFile = ofFile(ofToDataPath(fragmentShaderFilename));
+	geometryShaderFile = ofFile(ofToDataPath(geometryShaderFilename));
+
 	fileChangedTimes.clear();
-	fileChangedTimes.push_back( getLastModified( vertexShaderFile ) );
-	fileChangedTimes.push_back( getLastModified( fragmentShaderFile ) );
-	fileChangedTimes.push_back( getLastModified( geometryShaderFile ) );
-	
-	if( vertexShaderBuffer.size() > 0 )
-	{
-		setupShaderFromSource(GL_VERTEX_SHADER, vertexShaderBuffer.getText() );
-	}
+	fileChangedTimes.push_back(getLastModified(vertexShaderFile));
+	fileChangedTimes.push_back(getLastModified(fragmentShaderFile));
+	fileChangedTimes.push_back(getLastModified(geometryShaderFile));
 
-	if( fragmentShaderBuffer.size() > 0 )
-	{
-		setupShaderFromSource(GL_FRAGMENT_SHADER, fragmentShaderBuffer.getText());
-	}
+	if (!vertexShaderFilename.empty())
+		setupShaderFromFile(GL_VERTEX_SHADER, vertexShaderFilename);
 
-	#ifndef TARGET_OPENGLES
-	if( geometryShaderBuffer.size() > 0 )
-	{
-		setupShaderFromSource(GL_GEOMETRY_SHADER_EXT, geometryShaderBuffer.getText());
-	}
-	#endif
+	if (!fragmentShaderFilename.empty())
+		setupShaderFromFile(GL_FRAGMENT_SHADER, fragmentShaderFilename);
+
+#ifndef TARGET_OPENGLES
+	if (!geometryShaderFilename.empty())
+		setupShaderFromFile(GL_GEOMETRY_SHADER_EXT, geometryShaderFilename);
+#endif
 
 	bindDefaults();
-	
+
 	return linkProgram();
 }
 
@@ -101,98 +85,87 @@ bool ofxAutoReloadedShader::loadCompute(const std::filesystem::path& shaderName)
 	return ofShader::loadCompute(shaderName);
 }
 
-// ---------------------------------------------------------------------------------------------------------------------------------------------------
-//
 void ofxAutoReloadedShader::_update(ofEventArgs &e)
 {
-	if( loadShaderNextFrame )
+	if (loadShaderNextFrame)
 	{
 		reloadShaders();
 		loadShaderNextFrame = false;
 	}
-	
+
 	int currTime = ofGetElapsedTimeMillis();
-	
-	if( ((currTime - lastTimeCheckMillis) > millisBetweenFileCheck) &&
-	   !loadShaderNextFrame )
+
+	if (((currTime - lastTimeCheckMillis) > millisBetweenFileCheck) &&
+		!loadShaderNextFrame)
 	{
-		if( filesChanged() )
-		{
+		if (filesChanged())
 			loadShaderNextFrame = true;
-		}
-		
+
 		lastTimeCheckMillis = currTime;
 	}
 }
 
-
-// ---------------------------------------------------------------------------------------------------------------------------------------------------
-//
 bool ofxAutoReloadedShader::reloadShaders()
 {
 	if (type == TYPE::compute)
 		return loadCompute(computeShaderFilename);
 	else
-		return load( vertexShaderFilename,  fragmentShaderFilename, geometryShaderFilename );
+		return load(vertexShaderFilename, fragmentShaderFilename, geometryShaderFilename);
 }
 
-// ---------------------------------------------------------------------------------------------------------------------------------------------------
-//
 void ofxAutoReloadedShader::enableWatchFiles()
 {
-	if(!bWatchingFiles){
-		ofAddListener(ofEvents().update, this, &ofxAutoReloadedShader::_update );
+	if (!bWatchingFiles)
+	{
+		ofAddListener(ofEvents().update, this, &ofxAutoReloadedShader::_update);
 		bWatchingFiles = true;
 	}
 }
 
-// ---------------------------------------------------------------------------------------------------------------------------------------------------
-//
 void ofxAutoReloadedShader::disableWatchFiles()
 {
-	if(bWatchingFiles){
-		ofRemoveListener(ofEvents().update, this, &ofxAutoReloadedShader::_update );
+	if (bWatchingFiles)
+	{
+		ofRemoveListener(ofEvents().update, this, &ofxAutoReloadedShader::_update);
 		bWatchingFiles = false;
 	}
 }
 
-// ---------------------------------------------------------------------------------------------------------------------------------------------------
-//
 bool ofxAutoReloadedShader::filesChanged()
 {
 	bool fileChanged = false;
-	
-	if( vertexShaderFile.exists() )
+
+	if (vertexShaderFile.exists())
 	{
-		std::time_t vertexShaderFileLastChangeTime = getLastModified( vertexShaderFile );
-		if( vertexShaderFileLastChangeTime != fileChangedTimes.at(0) )
+		std::time_t vertexShaderFileLastChangeTime = getLastModified(vertexShaderFile);
+		if (vertexShaderFileLastChangeTime != fileChangedTimes.at(0))
 		{
 			fileChangedTimes.at(0) = vertexShaderFileLastChangeTime;
 			fileChanged = true;
 		}
 	}
-	
-	if( fragmentShaderFile.exists() )
+
+	if (fragmentShaderFile.exists())
 	{
-		std::time_t fragmentShaderFileLastChangeTime = getLastModified( fragmentShaderFile );
-		if( fragmentShaderFileLastChangeTime != fileChangedTimes.at(1) )
+		std::time_t fragmentShaderFileLastChangeTime = getLastModified(fragmentShaderFile);
+		if (fragmentShaderFileLastChangeTime != fileChangedTimes.at(1))
 		{
 			fileChangedTimes.at(1) = fragmentShaderFileLastChangeTime;
 			fileChanged = true;
 		}
 	}
-	
-	
-	if( geometryShaderFile.exists() )
+
+
+	if (geometryShaderFile.exists())
 	{
-		std::time_t geometryShaderFileLastChangeTime = getLastModified( geometryShaderFile );
-		if( geometryShaderFileLastChangeTime != fileChangedTimes.at(2) )
+		std::time_t geometryShaderFileLastChangeTime = getLastModified(geometryShaderFile);
+		if (geometryShaderFileLastChangeTime != fileChangedTimes.at(2))
 		{
 			fileChangedTimes.at(2) = geometryShaderFileLastChangeTime;
 			fileChanged = true;
 		}
 	}
-	
+
 
 	if (computeShaderFile.exists() && type == TYPE::compute)
 	{
@@ -203,45 +176,37 @@ bool ofxAutoReloadedShader::filesChanged()
 			fileChanged = true;
 		}
 	}
-	
+
 	return fileChanged;
 }
 
-// ---------------------------------------------------------------------------------------------------------------------------------------------------
-//
-std::time_t ofxAutoReloadedShader::getLastModified( ofFile& _file )
+std::time_t ofxAutoReloadedShader::getLastModified(ofFile& _file)
 {
-	if( _file.exists() )
-	{
-        return std::filesystem::last_write_time(_file.path());
-	}
+	if (_file.exists())
+		return std::filesystem::last_write_time(_file.path());
 	else
-	{
 		return 0;
-	}
 }
 
-// ---------------------------------------------------------------------------------------------------------------------------------------------------
-//
-void ofxAutoReloadedShader::setMillisBetweenFileCheck( int _millis )
+void ofxAutoReloadedShader::setMillisBetweenFileCheck(int _millis)
 {
 	millisBetweenFileCheck = _millis;
 }
 
-//--------------------------------------------------------------
-void ofxAutoReloadedShader::setGeometryInputType(GLenum type) {
-    ofShader::setGeometryInputType(type);
-    geometryInputType = type;
+void ofxAutoReloadedShader::setGeometryInputType(GLenum type) 
+{
+	ofShader::setGeometryInputType(type);
+	geometryInputType = type;
 }
 
-//--------------------------------------------------------------
-void ofxAutoReloadedShader::setGeometryOutputType(GLenum type) {
-    ofShader::setGeometryOutputType(type);
-    geometryOutputType = type;
+void ofxAutoReloadedShader::setGeometryOutputType(GLenum type) 
+{
+	ofShader::setGeometryOutputType(type);
+	geometryOutputType = type;
 }
 
-//--------------------------------------------------------------
-void ofxAutoReloadedShader::setGeometryOutputCount(int count) {
-    ofShader::setGeometryOutputCount(count);
-    geometryOutputCount = count;
+void ofxAutoReloadedShader::setGeometryOutputCount(int count) 
+{
+	ofShader::setGeometryOutputCount(count);
+	geometryOutputCount = count;
 }
--- src/ofxAutoReloadedShader.h
+++ src/ofxAutoReloadedShader.h
@@ -4,57 +4,47 @@
 
 class ofxAutoReloadedShader : public ofShader
 {
-	
-  public:
+
+public:
 	ofxAutoReloadedShader();
 	~ofxAutoReloadedShader();
-	
-	// override the initialisation functions
-	bool load(string shaderName );
-	bool load(string vertName, string fragName, string geomName="");
-	bool loadCompute(const std::filesystem::path& shaderName);
 
+	bool load(const std::filesystem::path& shaderName);
+	bool load(const std::filesystem::path& vertName, const std::filesystem::path& fragName, const std::filesystem::path& geomName = "");
+	bool loadCompute(const std::filesystem::path& shaderName);
 	bool reloadShaders();
-	
 	void enableWatchFiles();
 	void disableWatchFiles();
-		
-	void setMillisBetweenFileCheck( int _millis );
-	
+	void setMillisBetweenFileCheck(int _millis);
 	void _update(ofEventArgs &e);
-    
-    void setGeometryInputType(GLenum type);
-    void setGeometryOutputType(GLenum type);
-    void setGeometryOutputCount(int count);
-	
+	void setGeometryInputType(GLenum type);
+	void setGeometryOutputType(GLenum type);
+	void setGeometryOutputCount(int count);
+
 private:
-	
-	bool bWatchingFiles;
+
 	bool filesChanged();
-	
-	bool loadShaderNextFrame;
-	
-	std::time_t getLastModified( ofFile& _file );
+	std::time_t getLastModified(ofFile& _file);
 
 	enum struct TYPE { compute, other }; TYPE type;
-	
+
+	bool bWatchingFiles;
+	bool loadShaderNextFrame;
 	int lastTimeCheckMillis;
 	int millisBetweenFileCheck;
-	
-	string vertexShaderFilename;
-	string fragmentShaderFilename;
-	string geometryShaderFilename;
-	string computeShaderFilename;
-	
+
+	vector< std::time_t > fileChangedTimes;
+	std::filesystem::path vertexShaderFilename;
+	std::filesystem::path fragmentShaderFilename;
+	std::filesystem::path geometryShaderFilename;
+	std::filesystem::path computeShaderFilename;
 	ofFile vertexShaderFile;
 	ofFile fragmentShaderFile;
 	ofFile geometryShaderFile;
 	ofFile computeShaderFile;
-	
-	vector< std::time_t > fileChangedTimes;
-    
-    GLenum geometryInputType, geometryOutputType;
-    int geometryOutputCount;
+
+	GLenum geometryInputType, geometryOutputType;
+	int geometryOutputCount;
 };
 
 
